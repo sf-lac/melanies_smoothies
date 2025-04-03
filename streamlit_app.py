db82bedbc7a923f249479e4d7cab70ad79638c5a
sf-lac <lac@mechatronix.ca>
2025-04-03 14:56:38 -0700

    Update streamlit_app.py
    Added call to REST API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -47,4 +48,6 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-    
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
